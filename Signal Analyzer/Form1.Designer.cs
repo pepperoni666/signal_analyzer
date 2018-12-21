d1fc272c38bcb35207377122d31fcd769ed9d5e0
pepperoni666 <peretser08@gmail.com>
2018-12-21 11:44:17 +0100

    graph view and select file added

--- Signal Analyzer/Form1.Designer.cs
+++ Signal Analyzer/Form1.Designer.cs
@@ -28,13 +28,71 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.select_btn = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(12, 12);
+			this.chart1.Name = "chart1";
+			this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(760, 207);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// select_btn
+			// 
+			this.select_btn.Location = new System.Drawing.Point(778, 12);
+			this.select_btn.Name = "select_btn";
+			this.select_btn.Size = new System.Drawing.Size(172, 23);
+			this.select_btn.TabIndex = 1;
+			this.select_btn.Text = "Select file";
+			this.select_btn.UseVisualStyleBackColor = true;
+			this.select_btn.Click += new System.EventHandler(this.select_btn_Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(90, 259);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(673, 217);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(962, 497);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.select_btn);
+			this.Controls.Add(this.chart1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Button select_btn;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- Signal Analyzer/Form1.cs
+++ Signal Analyzer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,28 @@ namespace Signal_Analyzer
 {
 	public partial class Form1 : Form
 	{
+		private string fileName;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void select_btn_Click(object sender, EventArgs e)
+		{
+			var fileDialog = new OpenFileDialog();
+			DialogResult result = fileDialog.ShowDialog(); // Show the dialog.
+			if (result == DialogResult.OK) // Test result.
+			{
+				fileName = fileDialog.FileName;
+				try
+				{
+					label1.Text = File.ReadAllText(fileName);
+				}
+				catch (IOException)
+				{
+				}
+			}
 		}
 	}
 }
